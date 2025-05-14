c2a9a1b8f9c4b58bb826aa1599662442c2631291
Chicuno <computacionyoficina@hotmail.com>
2025-05-13 18:31:53 -0700

    Proyecto concluido con posibilidad de mejoras

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,8 @@
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
